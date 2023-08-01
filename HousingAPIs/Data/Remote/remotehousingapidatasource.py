cbd7f18cd2261940ce7095be0e1d6003d2ddf7fa
dz129 <danielzhang129@gmail.com>
2023-07-31 22:06:16 -0400

    50 meter radius thing works correctly

--- HousingAPIs/Data/Remote/remotehousingapidatasource.py
+++ HousingAPIs/Data/Remote/remotehousingapidatasource.py
@@ -1,18 +1,30 @@
-import overpy
 import requests
+import json
+import sys
+
+sys.path.append('HousingAPIs/Data/Remote/model')
+from model.houseresponse import HouseResponse
+
 
 class RemoteHousingAPIDataSource:
-
-    api = overpy.Overpass()
     """
     :param
     lat represents the lat of the center of the circle we are searching
     long represents the long of the center of the circle we are searching
     
     The return values should be a list of the houses in a 50 meter radius
+    probably want to map these values into a class
     """
+
     def getHouseAddresses50Meters(self, lat, long):
-        jsonResponse = requests.get(f"https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/{long},{lat}.json?radius=50&limit=50&dedupe&layers=housenum_label&access_token=pk.eyJ1IjoiZGFuaWVsemhhbmcxMjkiLCJhIjoiY2xrbGo0dW51MGI2eTNmcWVoYWRnZHlmeSJ9.BINJdFPV-t-SXnzr7AjuxA")
-        return jsonResponse
-
-
+        jsonResponse = requests.get(
+            f"https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/{long},{lat}.json?radius=50&limit=50&dedupe&layers=housenum_label&access_token=pk.eyJ1IjoiZGFuaWVsemhhbmcxMjkiLCJhIjoiY2xrbGo0dW51MGI2eTNmcWVoYWRnZHlmeSJ9.BINJdFPV-t-SXnzr7AjuxA").json()
+        houses = jsonResponse["features"]
+        # i want this HousesList to have a list fo house objects with lat and long, we can add the addresses later when we do the reverse geocoding
+        HousesList = []
+        for home in houses:
+            geometry = home["geometry"]
+            coordinates = geometry["coordinates"]
+            houseClass = HouseResponse(coordinates[1], coordinates[0])
+            HousesList.append(houseClass)
+        return HousesList
--- HousingAPIs/Domain/model/house.py
+++ HousingAPIs/Domain/model/house.py
@@ -1,18 +1,10 @@
 from dataclasses import dataclass
 
 @dataclass
-class House:
+class HouseLatLong:
     """
     First we will initalize the class with lat and long
     After that, we will reverse geocode and then add the features later
     """
     lat: float
     long: float
-    streetAddress: string
-    zip: string
-    city: string
-    country: string
-
-    def __init__(self, lat: float, long: float):
-        self.lat = lat
-        self.long = long
